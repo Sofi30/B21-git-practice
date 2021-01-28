09b78c849b893c1994b4d15a4c809a199b86951f
Sofi30 <sofiarozumny30@gmail.com>
2021-01-27 22:10:50 -0800

    master branch, changed movie location to bedroom

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -4,7 +4,7 @@ public class Hello {
 
         System.out.println("Hello World, B21");
         System.out.println("B21 is The Best");
-        // watching more movie
+        // watching more movie in bedroom
 
     }
 
